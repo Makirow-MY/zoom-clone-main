c965fecd7126fc7da531aa885062d287c0d5ab13
MAKIA YENGUE GODWILL <godwillmakiayengue@gmail.com>
2025-12-10 10:58:16 +0100

    helipluouuyuftugy

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,9 +1,6 @@
 import { ReactNode } from 'react';
 
 import StreamVideoProvider from '@/providers/StreamClientProvider';
-import { useUser } from '@clerk/nextjs';
-import { useRouter } from 'next/navigation';
-import SiginInPage from '../(auth)/sign-in/[[...sigin-in]]/page';
 
 const RootLayout = ({ children }: Readonly<{ children: ReactNode }>) => {
   return (
